d77623e6d2a03fc55cac634d1535d2b3a92becfd
MMcClure11 <mcclme02@luther.edu>
2021-03-25 14:20:52 -0500

    Add state and use onClickHandlers to update display of image for customer1

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import customer1Events from "./fake-charts/events-1.png";
-// import customer1Performance from "./fake-charts/perf-1.png";
+import customer1Performance from "./fake-charts/perf-1.png";
 import customer2Ftp from "./fake-charts/ftp-2.png";
 // import customer2Performance from "./fake-charts/perf-2.png";
 
 import "./styles.css";
 
 const Dashboard = () => {
+
+  const [customer1, setCustomer1] = useState('customer1Events')
+
+  const onClickHandler = () => {
+    setCustomer1('customer1Events')
+  }
+
+  const onClickHandler2 = () => {
+    setCustomer1('customer1Performance')
+  }
+
   return (
     <div>
       {/* Placeholder, feel free to edit the structure below */}
@@ -15,16 +26,20 @@ const Dashboard = () => {
         <h1>Customer 1</h1>
 
         <div className="tabs">
-          <div className="tab selected">
+          <div className="tab selected" onClick={ onClickHandler }>
             Search events
           </div>
-          <div className="tab">
+          <div className="tab" onClick={ onClickHandler2 }>
             Performance
           </div>
         </div>
 
         <div>
+          { customer1 === 'customer1Events' ?
             <img src={customer1Events} alt="Search events over time" />
+            :
+            <img src={customer1Performance} alt="Performance over time" />
+          }
         </div>
       </div>
 
